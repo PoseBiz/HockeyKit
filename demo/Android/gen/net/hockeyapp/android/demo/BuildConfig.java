206c151396ca7c3831c9d86bd33a64b30ec74c1c
Dean Singh <dean.singh@gmail.com>
2013-10-17 17:01:22 -0700

    Updated for android studio

--- demo/Android/gen/net/hockeyapp/android/demo/BuildConfig.java
+++ demo/Android/gen/net/hockeyapp/android/demo/BuildConfig.java
@@ -1,7 +1,7 @@
 /*___Generated_by_IDEA___*/
 
 /** Automatically generated file. DO NOT MODIFY */
-package net.hockeyapp.android;
+package net.hockeyapp.android.demo;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
